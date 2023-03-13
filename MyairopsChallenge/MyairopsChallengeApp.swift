6d2f05efa9ea487e0a5f804ba4c6236580c60851
Stebin Alex <stebinalex96@gmail.com>
2023-03-13 14:36:24 +0000

    Initial Commit

---
+++ MyairopsChallenge/MyairopsChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyairopsChallengeApp.swift
+//  MyairopsChallenge
+//
+//  Created by Stebin Alex on 13/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MyairopsChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
